b5faba630f068d1c31af2105e610d79c29256f66
jiahung rao <jiahungrao@gmail.com>
2020-04-09 17:19:12 +0800

    push

--- 登入註冊(AJAX-API)/AJAX-EX.js
+++ 登入註冊(AJAX-API)/AJAX-EX.js
@@ -1,5 +1,5 @@
-var JN = document.querySelector('.joinnow')
-var SI = document.querySelector('.signIn')
+let JN = document.querySelector('.joinnow')
+let SI = document.querySelector('.signIn')
 
 //監聽
 JN.addEventListener('click',JoinNow)
@@ -8,20 +8,20 @@ SI.addEventListener('click',SignIn)
 
 //註冊
 function JoinNow(){
-    var email = document.querySelector('.email').value
-    var password = document.querySelector('.password').value
-    var account = {}
+    let email = document.querySelector('.email').value
+    let password = document.querySelector('.password').value
+    let account = {}
     account.email = email
     account.password = password
 
-    var xhr = new XMLHttpRequest()
+    let xhr = new XMLHttpRequest()
     xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true)
     xhr.setRequestHeader('Content-type','application/json')
-    var data = JSON.stringify(account)
+    let data = JSON.stringify(account)
     xhr.send(data)
     xhr.onload = function(){
-        var callbackdata = JSON.parse(xhr.responseText) 
-        var callbackmsg = callbackdata.message
+        let callbackdata = JSON.parse(xhr.responseText) 
+        let callbackmsg = callbackdata.message
         if(callbackmsg == '帳號註冊成功'){
             alert('帳號註冊成功')
         }else{
@@ -33,20 +33,20 @@ function JoinNow(){
 
 //登入
 function SignIn(){
-    var email = document.querySelector('.email').value
-    var password = document.querySelector('.password').value
-    var account = {}
+    let email = document.querySelector('.email').value
+    let password = document.querySelector('.password').value
+    let account = {}
     account.email = email
     account.password = password
 
-    var xhr = new XMLHttpRequest()
+    let xhr = new XMLHttpRequest()
     xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signin',true)
     xhr.setRequestHeader('Content-type','application/json')
-    var data = JSON.stringify(account)
+    let data = JSON.stringify(account)
     xhr.send(data)
     xhr.onload = function(){
-        var callbackdata =JSON.parse(xhr.responseText)
-        var callbackmsg = callbackdata.message
+        let callbackdata =JSON.parse(xhr.responseText)
+        let callbackmsg = callbackdata.message
         if(callbackmsg == '登入成功'){
             alert('登入成功')
         }else{
